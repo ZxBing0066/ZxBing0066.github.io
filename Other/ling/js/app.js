66df183784cdd594e6129a3033749af8be843f12
ZxBing <8216822yu@sina.com>
2014-09-22 20:42:36 +0800

    更新题目
    Signed-off-by: ZxBing <8216822yu@sina.com>

--- Other/ling/js/app.js
+++ Other/ling/js/app.js
@@ -1,8 +1,12 @@
-$('#select ul').html(_.template('<%_.each(questions, function(question,index){%> <li index=<%=index%>><%=question.title%></li> <%})%>')({questions:questions}))
-
+$('#select ul').html(_.template('<%_.each(questions, function(question,index){%> <li index=<%=index%>><%=question.title%></li> <%})%>')({
+	questions: questions
+}))
 $('#select ul li').click(function() {
 	var index = $(this).attr('index');
 	if (index == $('#questions').attr('index')) {
+		return;
+	}
+	if ($('#questions').attr('index') != undefined && !confirm("Are you sure?")) {
 		return;
 	}
 	$('#questions').attr('index', index);
@@ -23,60 +27,63 @@ $('#retestBtn').click(retest);
 function render(index) {
 	$('#questions')[0].scrollTop = 0;
 	$('#questions>div').html(_.template("<h2><%=data.title%></h2>\
-		<%var letterArr = ['A', 'B', 'C', 'D', 'E', 'F']%>\
-		<%if(data.singleChoiceQ&&data.singleChoiceQ.length>0){%>\
-			<h4>单选题</h4>\
-			<ul class='singleChoice'>\
-			<%_.each(data.singleChoiceQ, function(question){%>\
-				<%if (question.a&&question.a.length>0) {%>\
-					<li data-answer=<%=question.a%>>\
-				<%} else {%>\
-					<li>\
-				<%}%>\
-				<p><%=question.q%></p>\
-				<div class='choices'>\
-				<%_.each(question.c, function(choice, index){%>\
-					<span><%=letterArr[index]%>、<%=choice%></span>\
-				<%})%>\
-				</div>\
-				<%if (question.a&&question.a.length>0) {%>\
-					<span class='answer'><%=question.a%></span>\
-				<%} else {%>\
-					<span class='answer'>未录入答案</span>\
-				<%}%>\
-				</li>\
+	<%var letterArr = ['A', 'B', 'C', 'D', 'E', 'F']%>\
+	<%if(data.singleChoiceQ&&data.singleChoiceQ.length>0){%>\
+		<h4>单选题</h4>\
+		<ul class='singleChoice'>\
+		<%_.each(data.singleChoiceQ, function(question){%>\
+			<%if (question.a&&question.a.length>0) {%>\
+				<li data-answer=<%=question.a%>>\
+			<%} else {%>\
+				<li>\
+			<%}%>\
+			<p><%=question.q%></p>\
+			<div class='choices'>\
+			<%_.each(question.c, function(choice, index){%>\
+				<span><%=letterArr[index]%>、<%=choice%></span>\
 			<%})%>\
-			</ul>\
-		<%}%>\
-		<%if(data.multipleChoiceQ&&data.multipleChoiceQ.length>0){%>\
-			<h4>多选题</h4>\
-			<ul class='multipleChoice'>\
-			<%_.each(data.multipleChoiceQ, function(question){%>\
-				<%if (question.a&&question.a.length>0) {%>\
-					<li data-answer=<%=question.a%>>\
-				<%} else {%>\
-					<li>\
-				<%}%>\
-				<p><%=question.q%></p>\
-				<div class='choices'>\
-				<%_.each(question.c, function(choice, index){%>\
-					<span><%=letterArr[index]%>、<%=choice%></span>\
-				<%})%>\
-				</div>\
-				<%if (question.a&&question.a.length>0) {%>\
-					<span class='answer'><%=question.a%></span>\
-				<%} else {%>\
-					<span class='answer'>未录入答案</span>\
-				<%}%>\
-				</li>\
-				<%})%>\
-			</ul>\
-		<%}%>")({
+			</div>\
+			<%if (question.a&&question.a.length>0) {%>\
+				<span class='answer'><%=question.a%></span>\
+			<%} else {%>\
+				<span class='answer'>未录入答案</span>\
+			<%}%>\
+			</li>\
+		<%})%>\
+		</ul>\
+	<%}%>\
+	<%if(data.multipleChoiceQ&&data.multipleChoiceQ.length>0){%>\
+		<h4>多选题</h4>\
+		<ul class='multipleChoice'>\
+		<%_.each(data.multipleChoiceQ, function(question){%>\
+			<%if (question.a&&question.a.length>0) {%>\
+				<li data-answer=<%=question.a%>>\
+			<%} else {%>\
+				<li>\
+			<%}%>\
+			<p><%=question.q%></p>\
+			<div class='choices'>\
+			<%_.each(question.c, function(choice, index){%>\
+				<span><%=letterArr[index]%>、<%=choice%></span>\
+			<%})%>\
+			</div>\
+			<%if (question.a&&question.a.length>0) {%>\
+				<span class='answer'><%=question.a%></span>\
+			<%} else {%>\
+				<span class='answer'>未录入答案</span>\
+			<%}%>\
+			</li>\
+			<%})%>\
+		</ul>\
+	<%}%>")({
 		data: questions[index]
 	}));
 }
 
 function showAnswer() {
+	if (!confirm("Are you sure?")) {
+		return;
+	}
 	$('.singleChoice li, .multipleChoice li').removeClass();
 	_.each($('.singleChoice li, .multipleChoice li'), function(li) {
 		var rightAnswer = $(li).data('answer');
@@ -105,9 +112,19 @@ function showAnswer() {
 }
 
 function retest() {
+	if (!confirm("Are you sure?")) {
+		return;
+	}
 	var index = $('#questions').attr('index');
-	if(index == null) {
+	if (index == null) {
 		return;
 	}
 	render(index);
+	$('#questions .singleChoice .choices>span').click(function(e) {
+		$(this).parent().find('.select').removeClass('select');
+		$(this).addClass('select');
+	})
+	$('#questions .multipleChoice .choices>span').click(function(e) {
+		$(this).toggleClass('select');
+	})
 }
